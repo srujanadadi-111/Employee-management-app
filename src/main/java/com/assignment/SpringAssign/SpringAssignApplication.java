78e8445b5f9e8675ec9a9c19a638c1c6d7c9fb28
srujanadadi-111 <srujana.dadi@egovernments.org>
2024-08-27 14:26:38 +0530

    Initial commit of Spring Boot application

---
+++ src/main/java/com/assignment/SpringAssign/SpringAssignApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.SpringAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAssignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/SpringAssign/SpringAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.SpringAssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
